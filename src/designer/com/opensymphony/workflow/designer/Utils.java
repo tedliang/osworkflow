5b7191469908da33c3eb73b85792c0d6d71a1538
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-05-21 22:08:39 +0000

    Initial import from SourceForge

---
+++ src/designer/com/opensymphony/workflow/designer/Utils.java
@@ -0,0 +1,96 @@
+package com.opensymphony.workflow.designer;
+
+import java.io.File;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+
+import com.jgraph.JGraph;
+
+/**
+ * @author Hani Suleiman (hani@formicary.net)
+ * Date: May 21, 2003
+ * Time: 12:12:44 AM
+ */
+public class Utils
+{
+  public static File promptUserForFile(Component component, int type, boolean save, final String suffix, final String description)
+  {
+    JFileChooser chooser = new JFileChooser(Prefs.INSTANCE.get(Prefs.CURRENT_DIR, System.getProperty("user.dir")));
+    chooser.rescanCurrentDirectory();
+    if(type == JFileChooser.FILES_AND_DIRECTORIES)
+    {
+      FileFilter filter = new FileFilter()
+      {
+        public boolean accept(File f)
+        {
+          return f.isDirectory() || f.getName().toLowerCase().endsWith(suffix);
+        }
+
+        public String getDescription()
+        {
+          return description;
+        }
+      };
+      chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+      chooser.setFileFilter(filter);
+    }
+    else
+    {
+      chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    }
+    File selectedFile = null;
+    int result;
+    if(save)
+    {
+      result = chooser.showSaveDialog(component);
+    }
+    else
+    {
+      result = chooser.showOpenDialog(component);
+    }
+
+    if(result == JFileChooser.APPROVE_OPTION)
+    {
+      selectedFile = chooser.getSelectedFile();
+      if(save && type==JFileChooser.FILES_AND_DIRECTORIES && !selectedFile.getName().toLowerCase().endsWith(suffix))
+      {
+        selectedFile = new File(selectedFile.toString() + suffix);
+      }
+    }
+    Prefs.INSTANCE.put(Prefs.CURRENT_DIR, chooser.getCurrentDirectory().toString());
+    return selectedFile;
+  }
+
+  public static BufferedImage toImage(JGraph graph)
+  {
+    Object[] cells = graph.getRoots();
+    if(cells.length > 0)
+    {
+      Rectangle bounds = graph.getCellBounds(cells);
+      graph.toScreen(bounds);
+
+      // Create a Buffered Image
+      Dimension d = bounds.getSize();
+      BufferedImage img = new BufferedImage(d.width + 10, d.height + 10, BufferedImage.TYPE_INT_RGB);
+      Graphics2D graphics = img.createGraphics();
+      graphics.setColor(graph.getBackground());
+      graphics.fillRect(0, 0, img.getWidth(), img.getHeight());
+      graphics.translate(-bounds.x + 5, -bounds.y + 5);
+
+      Object[] selection = graph.getSelectionCells();
+      boolean gridVisible = graph.isGridVisible();
+      graph.setGridVisible(false);
+      graph.clearSelection();
+
+      graph.paint(graphics);
+
+      graph.setSelectionCells(selection);
+      graph.setGridVisible(gridVisible);
+
+      return img;
+    }
+    return null;
+  }
+}
