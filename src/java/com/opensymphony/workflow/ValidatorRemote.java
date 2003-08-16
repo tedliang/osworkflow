4ed73a35094ebe70afd1d11eaba025935089d2d6
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-08-16 19:48:39 +0000

    Updated javadocs

--- src/java/com/opensymphony/workflow/ValidatorRemote.java
+++ src/java/com/opensymphony/workflow/ValidatorRemote.java
@@ -16,7 +16,7 @@ import java.util.Map;
  * Remote interface that must be implemented to define a java-based validator in your workflow definition.
  *
  * @author <a href="mailto:plightbo@hotmail.com">Patrick Lightbody</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public interface ValidatorRemote extends Remote {
     //~ Methods ////////////////////////////////////////////////////////////////
@@ -30,7 +30,7 @@ public interface ValidatorRemote extends Remote {
      * {@link com.opensymphony.workflow.spi.WorkflowEntry}) and <b>context</b>
      * (object type: {@link com.opensymphony.workflow.WorkflowContext}).
      * Also, any variable set as a {@link com.opensymphony.workflow.Register}), will also be
-     * available in the variable map, no matter what. These variables only last through
+     * available in the transient map, no matter what. These transient variables only last through
      * the method call that they were invoked in, such as {@link Workflow#initialize}
      * and {@link Workflow#doAction}.
      * @param args The properties for this function invocation. Properties are created
@@ -38,8 +38,8 @@ public interface ValidatorRemote extends Remote {
      * which is the properties key, and the CDATA text contents of the element map to
      * the property value.
      * @param ps The persistent variables that are associated with the current
-     * instance of the workflow. Any change made to this will be seen on the next
-     * function call in the workflow lifetime.
+     * instance of the workflow. Any change made to the propertyset are persisted to
+     * the propertyset implementation's persistent store.
      * @throws InvalidInputException if the input is deemed to be invalid
      */
     public void validate(Map transientVars, Map args, PropertySet ps) throws InvalidInputException, RemoteException;
