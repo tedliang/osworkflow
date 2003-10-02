971d189448a6584a99bdff4856d07e16816ca599
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-10-02 23:38:55 +0000

    Reworked to not require a net connection

--- trunk/src/test/com/opensymphony/workflow/SaveDescriptorTestCase.java
+++ trunk/src/test/com/opensymphony/workflow/SaveDescriptorTestCase.java
@@ -4,23 +4,25 @@
  */
 package com.opensymphony.workflow;
 
+import com.opensymphony.workflow.loader.DTDEntityResolver;
 import com.opensymphony.workflow.loader.WorkflowDescriptor;
-
-import junit.framework.Assert;
 
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.ElementNameAndTextQualifier;
 import org.custommonkey.xmlunit.XMLTestCase;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import org.w3c.dom.Document;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.*;
 
 import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 
 /**
@@ -33,10 +35,16 @@ import java.net.URL;
  * Time: 12:43:08 PM
  */
 public class SaveDescriptorTestCase extends XMLTestCase {
+    //~ Instance fields ////////////////////////////////////////////////////////
+
+    private DocumentBuilder documentBuilder;
+
     //~ Constructors ///////////////////////////////////////////////////////////
 
-    public SaveDescriptorTestCase(String s) {
+    public SaveDescriptorTestCase(String s) throws ParserConfigurationException {
         super(s);
+        documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        documentBuilder.setEntityResolver(new DTDEntityResolver());
     }
 
     //~ Methods ////////////////////////////////////////////////////////////////
@@ -53,10 +61,10 @@ public class SaveDescriptorTestCase extends XMLTestCase {
         writer.println("<!DOCTYPE workflow PUBLIC \"-//OpenSymphony Group//DTD OSWorkflow 2.5//EN\" \"http://www.opensymphony.com/osworkflow/workflow_2_5.dtd\">");
         descriptor.writeXML(new PrintWriter(out), 0);
 
-        String inputString = getFileAsString(url.getFile());
-        String outputString = out.toString();
+        Document inputDocument = documentBuilder.parse(url.toString());
+        Document outputDocument = getDocument(out.toString());
 
-        Diff diff = new Diff(inputString, outputString);
+        Diff diff = new Diff(inputDocument, outputDocument);
         diff.overrideElementQualifier(new ElementNameAndTextQualifier());
         assertTrue(diff.similar());
         assertXMLEqual("But they are equal when an ElementQualifier controls which test element is compared with each control element", diff, true);
@@ -64,28 +72,9 @@ public class SaveDescriptorTestCase extends XMLTestCase {
         assertTrue(diff.similar());
     }
 
-    private static String getFileAsString(String scriptFile) throws Exception {
-        File file = new File(scriptFile);
-        Assert.assertTrue(file.exists());
+    private Document getDocument(String xml) throws IOException, SAXException {
+        InputSource source = new InputSource(new StringReader(xml));
 
-        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-
-        return readTextStream(bis);
-    }
-
-    private static String readTextStream(InputStream is) throws Exception {
-        InputStreamReader reader = new InputStreamReader(is);
-        StringBuffer sb = new StringBuffer(100);
-        int c = 0;
-
-        while (c > -1) {
-            c = reader.read();
-
-            if (c > -1) {
-                sb.append((char) c);
-            }
-        }
-
-        return sb.toString();
+        return documentBuilder.parse(source);
     }
 }
