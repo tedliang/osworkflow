20bbdc58078f8b5ab96ad5e3b656d4d97a234cce
dep4b <dep4b@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-10-02 12:19:34 +0000

    Fix SaveDescriptorTestCase by using XML Unit to validate the sturcture instead of text matching.

--- src/test/com/opensymphony/workflow/SaveDescriptorTestCase.java
+++ src/test/com/opensymphony/workflow/SaveDescriptorTestCase.java
@@ -6,19 +6,33 @@ package com.opensymphony.workflow;
 
 import com.opensymphony.workflow.loader.WorkflowDescriptor;
 
-import junit.framework.TestCase;
+import junit.framework.Assert;
 
-import java.io.*;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.ElementNameAndTextQualifier;
+import org.custommonkey.xmlunit.XMLTestCase;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import java.net.URL;
 
 
 /**
+ * Test that when you save a descriptor, it is identical to the original.
+ * Identical means with the same xml syntax, not the exact same text wise.
+ *
  * @author Hani Suleiman (hani@formicary.net)
+ * @author Eric Pugh (epugh@upstate.com)
  * Date: May 5, 2003
  * Time: 12:43:08 PM
  */
-public class SaveDescriptorTestCase extends TestCase {
+public class SaveDescriptorTestCase extends XMLTestCase {
     //~ Constructors ///////////////////////////////////////////////////////////
 
     public SaveDescriptorTestCase(String s) {
@@ -31,7 +45,7 @@ public class SaveDescriptorTestCase extends TestCase {
      *Test if a saved descriptor is identical to the original
      */
     public void testSave() throws Exception {
-        URL url = getClass().getClassLoader().getResource("/samples/saved.xml");
+        URL url = getClass().getResource("/samples/saved.xml");
         WorkflowDescriptor descriptor = DescriptorLoader.getDescriptor(url.toString());
         StringWriter out = new StringWriter();
         PrintWriter writer = new PrintWriter(out);
@@ -39,9 +53,39 @@ public class SaveDescriptorTestCase extends TestCase {
         writer.println("<!DOCTYPE workflow PUBLIC \"-//OpenSymphony Group//DTD OSWorkflow 2.5//EN\" \"http://www.opensymphony.com/osworkflow/workflow_2_5.dtd\">");
         descriptor.writeXML(new PrintWriter(out), 0);
 
-        int origLength = (int) new File(url.getFile()).length();
-        int savedLength = out.toString().length();
-        int diff = Math.abs(origLength - savedLength);
-        assertEquals("Difference between saved and original is " + diff, diff < 4, true);
+        String inputString = getFileAsString(url.getFile());
+        String outputString = out.toString();
+
+        Diff diff = new Diff(inputString, outputString);
+        diff.overrideElementQualifier(new ElementNameAndTextQualifier());
+        assertTrue(diff.similar());
+        assertXMLEqual("But they are equal when an ElementQualifier controls which test element is compared with each control element", diff, true);
+
+        assertTrue(diff.similar());
+    }
+
+    private static String getFileAsString(String scriptFile) throws Exception {
+        File file = new File(scriptFile);
+        Assert.assertTrue(file.exists());
+
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+
+        return readTextStream(bis);
+    }
+
+    private static String readTextStream(InputStream is) throws Exception {
+        InputStreamReader reader = new InputStreamReader(is);
+        StringBuffer sb = new StringBuffer(100);
+        int c = 0;
+
+        while (c > -1) {
+            c = reader.read();
+
+            if (c > -1) {
+                sb.append((char) c);
+            }
+        }
+
+        return sb.toString();
     }
 }
