eb4218dd2fc4afc473a6aba0722998c267218ee9
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-10-07 02:21:51 +0000

    Fixed test script

--- trunk/src/etc/deployment/hibernate/mckoi.sql
+++ trunk/src/etc/deployment/hibernate/mckoi.sql
@@ -1,82 +1,24 @@
-
-DROP TABLE IF EXISTS OS_PROPERTYENTRY;
-DROP TABLE IF EXISTS OS_CURRENTSTEP_PREV;
-DROP TABLE IF EXISTS OS_HISTORYSTEP_PREV;
-DROP TABLE IF EXISTS OS_CURRENTSTEP;
-DROP TABLE IF EXISTS OS_HISTORYSTEP;
-DROP TABLE IF EXISTS OS_WFENTRY;
-DROP TABLE IF EXISTS hibernate_sequence;
-
-
-DROP SEQUENCE SEQ_OS_WFENTRY;
-DROP SEQUENCE SEQ_OS_CURRENTSTEPS;
-
-CREATE SEQUENCE SEQ_OS_WFENTRY INCREMENT 2 START 10;
-CREATE SEQUENCE SEQ_OS_CURRENTSTEPS INCREMENT 2 START 10;
-
-CREATE TABLE hibernate_sequence(
-   id NUMERIC
+alter table OS_HISTORYSTEP drop constraint historystep_entry_fk;
+alter table OS_CURRENTSTEP drop constraint currentstep_entry_fk;
+drop table OS_HISTORYSTEP;
+drop table OS_PROPERTYENTRY;
+drop table OS_WFENTRY;
+drop table OS_CURRENTSTEP;
+DROP TABLE hibernate_sequence;
+create table OS_HISTORYSTEP (
+   id BIGINT not null,
+   action_Id INTEGER,
+   caller VARCHAR(255),
+   finish_Date TIMESTAMP,
+   start_Date TIMESTAMP,
+   due_Date TIMESTAMP,
+   owner VARCHAR(255),
+   status VARCHAR(255),
+   step_Id INTEGER,
+   entry_Id BIGINT,
+   stepIndex INTEGER,
+   primary key (id)
 );
-
-CREATE TABLE OS_WFENTRY
-(
-    ID int,
-    NAME varchar(128),
-    STATE smallint,
-    primary key (ID)
-);
-
-CREATE TABLE OS_CURRENTSTEP
-(
-    ID int,
-    ENTRY_ID int,
-    STEP_ID smallint,
-    ACTION_ID smallint,
-    OWNER varchar(20),
-    START_DATE TIMESTAMP ,
-    FINISH_DATE TIMESTAMP ,
-    DUE_DATE TIMESTAMP ,
-    STATUS varchar(20),
-    CALLER varchar(20),
-    primary key (ID),
-    foreign key (ENTRY_ID) references OS_WFENTRY(ID)
-   
-);
-
-CREATE TABLE OS_HISTORYSTEP
-(
-    ID int,
-    ENTRY_ID int,
-    STEP_ID smallint,
-    ACTION_ID smallint,
-    OWNER varchar(20),
-    START_DATE TIMESTAMP ,
-    FINISH_DATE TIMESTAMP ,
-    DUE_DATE TIMESTAMP ,
-    STATUS varchar(20),
-    CALLER varchar(20),
-    primary key (ID),
-    foreign key (ENTRY_ID) references OS_WFENTRY(ID)
-);
-
-CREATE TABLE OS_CURRENTSTEP_PREV
-(
-    ID int,
-    PREVIOUS_ID int,
-    primary key (ID, PREVIOUS_ID),
-    foreign key (ID) references OS_CURRENTSTEP(ID),
-    foreign key (PREVIOUS_ID) references OS_HISTORYSTEP(ID)
-);
-
-CREATE TABLE OS_HISTORYSTEP_PREV
-(
-    ID int,
-    PREVIOUS_ID int,
-    primary key (ID, PREVIOUS_ID),
-    foreign key (ID) references OS_HISTORYSTEP(ID),
-    foreign key (PREVIOUS_ID) references OS_HISTORYSTEP(ID)
-);
-
 create table OS_PROPERTYENTRY (
    entity_name VARCHAR(125) not null,
    entity_id BIGINT not null,
@@ -90,6 +32,28 @@ create table OS_PROPERTYENTRY (
    date_val DATE,
    primary key (entity_name, entity_id, entity_key)
 );
-
-
-
+create table OS_WFENTRY (
+   id BIGINT not null,
+   name VARCHAR(255),
+   state INTEGER,
+   primary key (id)
+);
+create table OS_CURRENTSTEP (
+   id BIGINT not null,
+   action_Id INTEGER,
+   caller VARCHAR(255),
+   finish_Date TIMESTAMP,
+   start_Date TIMESTAMP,
+   due_Date TIMESTAMP,
+   owner VARCHAR(255),
+   status VARCHAR(255),
+   step_Id INTEGER,
+   entry_Id BIGINT,
+   stepIndex INTEGER,
+   primary key (id)
+);
+alter table OS_HISTORYSTEP add constraint historystep_entry_fk foreign key (entry_Id) references OS_WFENTRY;
+alter table OS_CURRENTSTEP add constraint currentstep_entry_fk foreign key (entry_Id) references OS_WFENTRY;
+CREATE TABLE hibernate_sequence(
+   id NUMERIC
+);
