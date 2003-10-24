07ed04a780ce95eeac777eceaaa676b1f7e9ea98
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-10-24 18:36:21 +0000

    Placeholder

---
+++ trunk/src/designer/com/opensymphony/workflow/designer/actions/CreateInitialAction.java
@@ -0,0 +1,24 @@
+package com.opensymphony.workflow.designer.actions;
+
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+import com.opensymphony.workflow.designer.WorkflowGraph;
+
+/**
+ * User: Hani Suleiman
+ * Date: Oct 24, 2003
+ * Time: 2:29:02 PM
+ */
+public class CreateInitialAction extends AbstractAction
+{
+  public CreateInitialAction(WorkflowGraph graph)
+  {
+    super("Initial Action");
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    //To change body of implemented methods use Options | File Templates.
+  }
+}
