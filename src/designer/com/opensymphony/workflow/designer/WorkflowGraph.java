884be1bdb28c494f6a0c1d2d89976ed024d1a0cf
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-10-24 18:36:02 +0000

    Add ability to drag connectors, not hooked in properly yet

--- src/designer/com/opensymphony/workflow/designer/WorkflowGraph.java
+++ src/designer/com/opensymphony/workflow/designer/WorkflowGraph.java
@@ -13,12 +13,14 @@ import com.opensymphony.workflow.loader.*;
 import com.opensymphony.workflow.designer.layout.SugiyamaLayoutAlgorithm;
 import com.opensymphony.workflow.designer.layout.LayoutAlgorithm;
 import com.opensymphony.workflow.designer.views.*;
+import com.opensymphony.workflow.designer.actions.CreateInitialAction;
 
 public class WorkflowGraph extends JGraph
 {
   private Layout layout = new Layout();
 
   private WorkflowDescriptor descriptor;
+  private JPopupMenu menu;
 
   public WorkflowGraph(GraphModel model, WorkflowDescriptor descriptor, Layout layout, boolean doAutoLayout)
   {
@@ -45,6 +47,10 @@ public class WorkflowGraph extends JGraph
     setMarqueeHandler(new WorkflowMarqueeHandler());
     setCloneable(false);
     setPortsVisible(true);
+    menu = new JPopupMenu();
+    JMenu n = new JMenu("New");
+    menu.add(n);
+    n.add(new CreateInitialAction(this));
   }
 
   protected PortView createPortView(Object object, CellMapper mapper)
@@ -54,10 +60,60 @@ public class WorkflowGraph extends JGraph
 
   public class WorkflowMarqueeHandler extends BasicMarqueeHandler
   {
-    public void mouseReleased(MouseEvent e)
+    // Holds the Start and the Current Point
+    protected Point start, current;
+
+    // Holds the First and the Current Port
+    protected PortView port, firstPort;
+
+    // Override to Gain Control (for PopupMenu and ConnectMode)
+    public boolean isForceMarqueeEvent(MouseEvent e)
     {
-      repaint();
-      super.mouseReleased(e);
+      // If Right Mouse Button we want to Display the PopupMenu
+      if(SwingUtilities.isRightMouseButton(e))
+      // Return Immediately
+        return true;
+      // Find and Remember Port
+      port = getSourcePortAt(e.getPoint());
+      // If Port Found and in ConnectMode (=Ports Visible)
+      if(port != null && isPortsVisible())
+        return true;
+      // Else Call Superclass
+      return super.isForceMarqueeEvent(e);
+    }
+
+    // Use Xor-Mode on Graphics to Paint Connector
+    protected void paintConnector(Color fg, Color bg, Graphics g)
+    {
+      // Set Foreground
+      g.setColor(fg);
+      // Set Xor-Mode Color
+      g.setXORMode(bg);
+      // Highlight the Current Port
+      paintPort(getGraphics());
+      // If Valid First Port, Start and Current Point
+      if(firstPort != null && start != null && current != null)
+      // Then Draw A Line From Start to Current Point
+        g.drawLine(start.x, start.y, current.x, current.y);
+    }
+
+    // Use the Preview Flag to Draw a Highlighted Port
+    protected void paintPort(Graphics g)
+    {
+      // If Current Port is Valid
+      if(port != null)
+      {
+        // If Not Floating Port...
+        boolean o = (GraphConstants.getOffset(port.getAttributes()) != null);
+        // ...Then use Parent's Bounds
+        Rectangle r = (o) ? port.getBounds() : port.getParentView().getBounds();
+        // Scale from Model to Screen
+        r = toScreen(new Rectangle(r));
+        // Add Space For the Highlight Border
+        r.setBounds(r.x - 3, r.y - 3, r.width + 6, r.height + 6);
+        // Paint Port in Preview (=Highlight) Mode
+        getUI().paintCell(g, port, r, true);
+      }
     }
 
     public PortView getSourcePortAt(Point point)
@@ -66,6 +122,112 @@ public class WorkflowGraph extends JGraph
       Point tmp = fromScreen(new Point(point));
       // Find a Port View in Model Coordinates and Remember
       return getPortViewAt(tmp.x, tmp.y);
+    }
+
+    // Find a Cell at point and Return its first Port as a PortView
+    protected PortView getTargetPortAt(Point point)
+    {
+      // Find Cell at point (No scaling needed here)
+      Object cell = getFirstCellForLocation(point.x, point.y);
+      // Loop Children to find PortView
+      for(int i = 0; i < getModel().getChildCount(cell); i++)
+      {
+        // Get Child from Model
+        Object tmp = getModel().getChild(cell, i);
+        // Get View for Child using the Graph's View as a Cell Mapper
+        tmp = getGraphLayoutCache().getMapping(tmp, false);
+        // If Child View is a Port View and not equal to First Port
+        if(tmp instanceof PortView && tmp != firstPort)
+        // Return as PortView
+          return (PortView)tmp;
+      }
+      // No Port View found
+      return getSourcePortAt(point);
+    }
+
+// Display PopupMenu or Remember Start Location and First Port
+    public void mousePressed(final MouseEvent e)
+    {
+      // If Right Mouse Button
+      if(SwingUtilities.isRightMouseButton(e))
+      {
+        // Scale From Screen to Model
+        Point loc = fromScreen(e.getPoint());
+        // Find Cell in Model Coordinates
+        Object cell = getFirstCellForLocation(loc.x, loc.y);
+        // Create PopupMenu for the Cell
+        menu.show(WorkflowGraph.this, e.getX(), e.getY());
+//				JPopupMenu menu = createPopupMenu(e.getPoint(), cell);
+//				menu.show(graph, e.getX(), e.getY());
+
+        // Else if in ConnectMode and Remembered Port is Valid
+      }
+      else if(port != null && !e.isConsumed() && isPortsVisible())
+      {
+        // Remember Start Location
+        start = toScreen(port.getLocation(null));
+        // Remember First Port
+        firstPort = port;
+        // Consume Event
+        e.consume();
+      }
+      else
+      // Call Superclass
+        super.mousePressed(e);
+    }
+
+    // Find Port under Mouse and Repaint Connector
+    public void mouseDragged(MouseEvent e)
+    {
+      // If remembered Start Point is Valid
+      if(start != null && !e.isConsumed())
+      {
+        // Fetch Graphics from Graph
+        Graphics g = getGraphics();
+        // Xor-Paint the old Connector (Hide old Connector)
+        paintConnector(Color.black, getBackground(), g);
+        // Reset Remembered Port
+        PortView newPort = getTargetPortAt(e.getPoint());
+        if(port!=newPort && port!=null)
+        {
+          paintPort(g);
+        }
+        port = newPort;
+        // If Port was found then Point to Port Location
+        if(port != null)
+          current = toScreen(port.getLocation(null));
+        // Else If no Port was found then Point to Mouse Location
+        else
+          current = snap(e.getPoint());
+        // Xor-Paint the new Connector
+        paintConnector(getBackground(), Color.black, g);
+        // Consume Event
+        e.consume();
+      }
+      // Call Superclass
+      super.mouseDragged(e);
+    }
+
+    // Connect the First Port and the Current Port in the Graph or Repaint
+    public void mouseReleased(MouseEvent e)
+    {
+      // If Valid Event, Current and First Port
+      if(e != null && !e.isConsumed() && port != null && firstPort != null && firstPort != port)
+      {
+        // Then Establish Connection
+        System.out.println("connect " + firstPort.getCell() + "->" + port.getCell());
+//				connect((Port) firstPort.getCell(), (Port) port.getCell());
+        // Consume Event
+        e.consume();
+        // Else Repaint the Graph
+      }
+      else
+        repaint();
+      // Reset Global Vars
+      firstPort = port = null;
+      start = current = null;
+      // Call Superclass
+      super.mouseReleased(e);
     }
 
     // Show Special Cursor if Over Port
@@ -249,7 +411,7 @@ public class WorkflowGraph extends JGraph
     getWorkflowGraphModel().insertJoin(join, null, null, null);
   }
 
-  private void addInitialActionsCell(InitialActionCell initialActionCell)
+  public void addInitialActionsCell(InitialActionCell initialActionCell)
   {
     if(layout != null)
     {
