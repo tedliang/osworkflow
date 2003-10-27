d913b0747e59202e685d5746d49d0afb08c0b3f4
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-10-27 18:21:56 +0000

    initial revision

---
+++ src/designer/com/opensymphony/workflow/designer/actions/CreateStep.java
@@ -0,0 +1,42 @@
+package com.opensymphony.workflow.designer.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.*;
+import javax.swing.*;
+
+import com.opensymphony.workflow.designer.*;
+import com.opensymphony.workflow.loader.StepDescriptor;
+import org.jgraph.graph.GraphConstants;
+
+/**
+ * User: Hani Suleiman
+ * Date: Oct 24, 2003
+ * Time: 2:29:02 PM
+ */
+public class CreateStep extends AbstractAction
+{
+  private WorkflowGraphModel model;
+  private Point location;
+
+  public CreateStep(WorkflowGraphModel model, Point location)
+  {
+    super("Step");
+    this.model = model;
+    this.location = location;
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    StepDescriptor step = new StepDescriptor();
+    String name = JOptionPane.showInputDialog("Step Name?");
+    if(name==null || name.trim().length()==0) return;
+    step.setName(name);
+    StepCell cell = new StepCell(step);
+    WorkflowPort port = new WorkflowPort();
+    cell.add(port);
+    Rectangle bounds = (Rectangle)cell.getAttributes().get(GraphConstants.BOUNDS);
+    bounds.x = location.x;
+    bounds.y = location.y;
+    model.insertStepCell(cell, null, null, null);
+  }
+}
