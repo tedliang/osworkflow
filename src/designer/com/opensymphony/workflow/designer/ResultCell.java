ebd7e8e9ef0ff11ae7e1026dad48ef477bcc6555
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-10-27 18:22:36 +0000

    Store ActionDescriptor

--- src/designer/com/opensymphony/workflow/designer/ResultCell.java
+++ src/designer/com/opensymphony/workflow/designer/ResultCell.java
@@ -6,19 +6,23 @@ package com.opensymphony.workflow.designer;
 
 import org.jgraph.graph.DefaultGraphCell;
 import com.opensymphony.workflow.loader.ResultDescriptor;
+import com.opensymphony.workflow.loader.ActionDescriptor;
 
 public class ResultCell extends WorkflowCell implements Keyable
 {
   private DefaultGraphCell fromCell;
   private ResultDescriptor descriptor;
+  //the action that causes this result
+  private ActionDescriptor action;
 
   //   private String mKey;
-  public ResultCell(DefaultGraphCell fromCell, ResultDescriptor resultDescriptor, String actionName)
+  public ResultCell(DefaultGraphCell fromCell, ResultDescriptor resultDescriptor, ActionDescriptor action)
   {
-    super(actionName);
+    super(action);
     this.fromCell = fromCell;
     descriptor = resultDescriptor;
     id = descriptor.getId();
+    this.action = action;
   }
 
   public String getKey()
@@ -49,5 +53,10 @@ public class ResultCell extends WorkflowCell implements Keyable
   public int getJoin()
   {
     return descriptor.getJoin();
+  }
+
+  public ActionDescriptor getAction()
+  {
+    return action;
   }
 }
