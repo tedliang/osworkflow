c55f6e8fc60662d24a88ad4f723bd6b4df834709
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-10-28 19:23:45 +0000

    More javadocs

--- src/java/com/opensymphony/workflow/query/WorkflowExpressionQuery.java
+++ src/java/com/opensymphony/workflow/query/WorkflowExpressionQuery.java
@@ -13,7 +13,7 @@ package com.opensymphony.workflow.query;
  * whereby all expressions must return true for a result to be included.
  *
  * @author Christine Zimmermann
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class WorkflowExpressionQuery {
     //~ Static fields/initializers /////////////////////////////////////////////
@@ -37,10 +37,18 @@ public class WorkflowExpressionQuery {
 
     //~ Constructors ///////////////////////////////////////////////////////////
 
+    /**
+     * Create a WorkflowExpressionQuery that consists of one expression.
+     */
     public WorkflowExpressionQuery(Expression expression) {
         expressions = new Expression[] {expression};
     }
 
+    /**
+     * Create a WorkflowExpressionQuery that consists of multiple expressions.
+     * @param expressions an array of expressions for this query.
+     * @param operator {@link WorkflowExpressionQuery#AND} or {@link WorkflowExpressionQuery#OR}.
+     */
     public WorkflowExpressionQuery(Expression[] expressions, int operator) {
         this.expressions = expressions;
         this.operator = operator;
@@ -52,10 +60,17 @@ public class WorkflowExpressionQuery {
         return expressions[index];
     }
 
+    /**
+     * Get the number of expressions in this query.
+     */
     public int getExpressionCount() {
         return expressions.length;
     }
 
+    /**
+     * @return {@link WorkflowExpressionQuery#AND} if all the expressions must match,
+     * or {@link WorkflowExpressionQuery#OR} if only one must match.
+     */
     public int getOperator() {
         return this.operator;
     }
