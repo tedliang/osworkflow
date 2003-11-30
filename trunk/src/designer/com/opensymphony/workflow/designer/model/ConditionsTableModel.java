9112254a4ca3c0b5afa210d6941608dd6129f6c8
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-11-30 22:38:20 +0000

    Just show name and type

--- trunk/src/designer/com/opensymphony/workflow/designer/model/ConditionsTableModel.java
+++ trunk/src/designer/com/opensymphony/workflow/designer/model/ConditionsTableModel.java
@@ -9,11 +9,11 @@ import com.opensymphony.workflow.loader.ConditionDescriptor;
  */
 public class ConditionsTableModel extends ListTableModel
 {
-  private String[] header = new String[]{"id", "type", "negate"};
+  private String[] header = new String[]{"name", "type"};
 
   public boolean isCellEditable(int rowIndex, int columnIndex)
   {
-    return true;
+    return false;
   }
 
   public void setValueAt(Object aValue, int rowIndex, int columnIndex)
@@ -22,15 +22,12 @@ public class ConditionsTableModel extends ListTableModel
     switch(columnIndex)
     {
       case 0:
-        if(aValue!=null)
-          condition.setId(((Integer)aValue).intValue());
+        if(aValue != null)
+          condition.setName(aValue.toString());
         break;
       case 1:
-        if(aValue!=null)
+        if(aValue != null)
           condition.setType(aValue.toString());
-        break;
-      case 2:
-        condition.setNegate(((Boolean)aValue).booleanValue());
         break;
     }
   }
@@ -47,17 +44,7 @@ public class ConditionsTableModel extends ListTableModel
 
   public Class getColumnClass(int columnIndex)
   {
-    switch(columnIndex)
-    {
-      case 0:
-        return Integer.class;
-      case 1:
-        return String.class;
-      case 2:
-        return Boolean.class;
-      default:
-        return String.class;
-    }
+    return String.class;
   }
 
   public Object getValueAt(int rowIndex, int columnIndex)
@@ -66,11 +53,10 @@ public class ConditionsTableModel extends ListTableModel
     switch(columnIndex)
     {
       case 0:
-        return condition.hasId() ? new Integer(condition.getId()) : null;
+        return condition.getName();
       case 1:
         return condition.getType();
-      default:
-        return condition.isNegate() ? Boolean.TRUE : Boolean.FALSE;
     }
+    return null;
   }
 }
