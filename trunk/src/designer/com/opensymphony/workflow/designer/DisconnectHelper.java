19cdfc75056f0c7cc57098e504bb8e646bc44492
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-11-30 22:49:22 +0000

    Helper classes to handle connection/disconnection of nodes

---
+++ trunk/src/designer/com/opensymphony/workflow/designer/DisconnectHelper.java
@@ -0,0 +1,25 @@
+/*
+ * Created on 2003-11-20
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.opensymphony.workflow.designer;
+
+import com.opensymphony.workflow.loader.ResultDescriptor;
+
+/**
+ * @author jackflit
+ * Date: 2003-11-20
+ */
+public class DisconnectHelper {
+	
+	public static boolean disconnectEdge(ResultEdge edge, WorkflowGraphModel model){
+		ResultDescriptor result = edge.getDescriptor();
+		
+//		edge.get
+		
+		return true;
+	}
+
+}
