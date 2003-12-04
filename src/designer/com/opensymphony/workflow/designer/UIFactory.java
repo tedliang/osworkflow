8709db6ba1f10dc40d246529766fbd7a635acecd
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-12-04 04:58:45 +0000

    Added more factory methods

--- src/designer/com/opensymphony/workflow/designer/UIFactory.java
+++ src/designer/com/opensymphony/workflow/designer/UIFactory.java
@@ -1,16 +1,15 @@
 package com.opensymphony.workflow.designer;
 
+import java.awt.*;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import com.jgoodies.forms.builder.ButtonBarBuilder;
 
 /**
  * @author Hani Suleiman (hani@formicary.net)
- * Date: May 21, 2003
- * Time: 9:46:11 PM
+ *         Date: May 21 2003
+ *         Time: 9:46:11 PM
  */
 public class UIFactory
 {
@@ -29,5 +28,46 @@ public class UIFactory
     ButtonBarBuilder builder = new ButtonBarBuilder(panel);
     builder.addGriddedGrowingButtons(buttons);
     return panel;
+  }
+
+  public static JComponent createTablePanel(JTable table)
+  {
+    Color background = UIManager.getColor("window");
+    JScrollPane scrollPane = new JScrollPane(table);
+    scrollPane.getViewport().setOpaque(false);
+    scrollPane.getViewport().setBackground(background);
+    scrollPane.setOpaque(false);
+    scrollPane.setCorner(JScrollPane.UPPER_RIGHT_CORNER, new JPanel());
+    scrollPane.setCorner(JScrollPane.LOWER_RIGHT_CORNER, new JPanel());
+    JPanel panel = new JPanel(new GridLayout(0, 1))
+    {
+      public void updateUI()
+      {
+        super.updateUI();
+        setBackground(UIManager.getColor("window"));
+      }
+    };
+    panel.setPreferredSize(new Dimension(220, 100));
+    panel.add(scrollPane);
+    return panel;
+  }
+
+  public static JTextField createReadOnlyTextField(int width)
+  {
+    JTextField field = new JTextField(width)
+    {
+      public boolean isFocusable()
+      {
+        return false;
+      }
+
+      protected void processEvent(AWTEvent e)
+      {
+      }
+    };
+    field.setEditable(false);
+    field.setOpaque(false);
+    field.setForeground(Color.black);
+    return field;
   }
 }
