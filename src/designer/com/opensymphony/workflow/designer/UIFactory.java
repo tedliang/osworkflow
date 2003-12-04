f7b7385ec372b1d12be8b1729c033ac19b5e09ec
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-12-04 04:37:19 +0000

    no message

--- src/designer/com/opensymphony/workflow/designer/UIFactory.java
+++ src/designer/com/opensymphony/workflow/designer/UIFactory.java
@@ -14,28 +14,6 @@ import com.jgoodies.forms.builder.ButtonBarBuilder;
  */
 public class UIFactory
 {
-  public static JPanel getTableButtonBar(ActionListener listener, String prefix)
-  {
-    if(prefix == null) prefix = "";
-    JButton[] buttons = new JButton[3];
-    JButton add = new JButton("Add");
-    add.setActionCommand(prefix + "add");
-    buttons[0] = add;
-    add.addActionListener(listener);
-    JButton remove = new JButton("Remove");
-    remove.setActionCommand(prefix + "remove");
-    remove.addActionListener(listener);
-    buttons[1] = remove;
-    JButton props = new JButton("Properties...");
-    props.setActionCommand(prefix + "properties");
-    props.addActionListener(listener);
-    buttons[2] = props;
-    JPanel panel = new JPanel();
-    ButtonBarBuilder builder = new ButtonBarBuilder(panel);
-    builder.addGriddedGrowingButtons(buttons);
-    return panel;
-  }
-
   public static JPanel getTableButtonBar(ActionListener listener, String prefix, String[] names)
   {
     if(prefix == null) prefix = "";
@@ -45,13 +23,11 @@ public class UIFactory
       buttons[i] = new JButton(names[i]);
       buttons[i].setActionCommand(prefix + names[i]);
       buttons[i].addActionListener(listener);
-
     }
 
     JPanel panel = new JPanel();
     ButtonBarBuilder builder = new ButtonBarBuilder(panel);
     builder.addGriddedGrowingButtons(buttons);
-    //	panel.setPreferredSize(new Dimension(200, 100));
     return panel;
   }
 }
