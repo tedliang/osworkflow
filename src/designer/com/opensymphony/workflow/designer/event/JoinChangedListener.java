bdfc99a7cf75b1b7d4f28a77dd574523a7fc32b2
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-12-04 03:32:19 +0000

    Reformatting crap

--- src/designer/com/opensymphony/workflow/designer/event/JoinChangedListener.java
+++ src/designer/com/opensymphony/workflow/designer/event/JoinChangedListener.java
@@ -1,18 +1,13 @@
 /*
  * Created on 2003-11-22
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
 package com.opensymphony.workflow.designer.event;
 
 /**
  * @author Gulei
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public interface JoinChangedListener {
-	public void joinChanged(JoinChangedEvent event);
+public interface JoinChangedListener
+{
+  public void joinChanged(JoinChangedEvent event);
 
 }
