8aba7f1b5b0d597bab9b92e01be25b1a64822f57
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-12-06 03:57:27 +0000

    Cleanup

--- trunk/src/designer/com/opensymphony/workflow/designer/views/JoinView.java
+++ trunk/src/designer/com/opensymphony/workflow/designer/views/JoinView.java
@@ -1,7 +1,3 @@
-/**
- * Created on Feb 13, 2003
- * Copyright (C) 2002  Aditisoft Inc
- */
 package com.opensymphony.workflow.designer.views;
 
 import org.jgraph.JGraph;
@@ -13,13 +9,9 @@ public class JoinView extends VertexView
 {
   private static final JoinRenderer renderer = new JoinRenderer();
 
-  // Constructor for Superclass
-  private Object cell;
-
   public JoinView(Object cell, JGraph graph, CellMapper mapper)
   {
     super(cell, graph, mapper);
-    this.cell = cell;
   }
 
   // Returns the Renderer for this View
