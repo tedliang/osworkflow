52581113346f67f9d65128d110400870ad295c0b
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-12-28 22:13:50 +0000

    If no current workspace on startup, prompt user to load/create

---
+++ src/designer/com/opensymphony/workflow/designer/dialogs/NewWorkspaceDialog.java
@@ -0,0 +1,112 @@
+package com.opensymphony.workflow.designer.dialogs;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import javax.swing.*;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.opensymphony.workflow.designer.swing.DefaultFormBuilder;
+import com.opensymphony.workflow.designer.swing.FileField;
+import com.opensymphony.workflow.designer.*;
+import com.opensymphony.workflow.loader.Workspace;
+
+/**
+ * @author Hani Suleiman (hani@formicary.net)
+ *         Date: Dec 28, 2003
+ *         Time: 4:39:11 PM
+ */
+public class NewWorkspaceDialog extends JDialog implements ActionListener
+{
+  private JRadioButton create = new JRadioButton(ResourceManager.getString("workspace.create"));
+  private JRadioButton load = new JRadioButton(ResourceManager.getString("workspace.load"));
+  private FileField loadField = new FileField(JFileChooser.FILES_AND_DIRECTORIES, false, WorkflowDesigner.WORKSPACE_SUFFIX, ResourceManager.getString("workspace.files"));
+  private FileField createField = new FileField(JFileChooser.FILES_AND_DIRECTORIES, true, WorkflowDesigner.WORKSPACE_SUFFIX, ResourceManager.getString("workspace.files"));
+
+  public NewWorkspaceDialog(Frame owner, String title, boolean modal) throws HeadlessException
+  {
+    super(owner, title, modal);
+    getContentPane().setLayout(new BorderLayout());
+    FormLayout layout = new FormLayout("2dlu, left:max(40dlu;pref), 3dlu, 110dlu:grow, 7dlu");
+    DefaultFormBuilder builder = new DefaultFormBuilder(layout, ResourceManager.getBundle());
+    builder.setLeadingColumnOffset(1);
+
+    builder.appendI15dSeparator("workspace.createload");
+    builder.append(load, loadField);
+    loadField.addFocusListener(new FocusAdapter()
+    {
+      public void focusGained(FocusEvent e)
+      {
+        load.setSelected(true);
+      }
+    });
+    loadField.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        load.setSelected(true);
+      }
+    });
+    builder.nextLine();
+    ButtonGroup group = new ButtonGroup();
+    group.add(create);
+    group.add(load);
+    builder.append(create, createField);
+    builder.nextLine();
+    createField.getTextField().addFocusListener(new FocusAdapter()
+    {
+      public void focusGained(FocusEvent e)
+      {
+        create.setSelected(true);
+      }
+    });
+    createField.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        create.setSelected(true);
+      }
+    });
+
+    getContentPane().add(builder.getPanel(), BorderLayout.CENTER);
+    getContentPane().add(UIFactory.getButtonBar(this, "", new String[]{"ok", "cancel"}), BorderLayout.SOUTH);
+    pack();
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    String command = e.getActionCommand();
+    if("ok".equals(command))
+    {
+      if(load.isSelected())
+      {
+        File file = loadField.getFile();
+        if(file==null || file.isDirectory())
+        {
+          JOptionPane.showMessageDialog((Component)e.getSource(), ResourceManager.getString("error.file.invalid"), ResourceManager.getString("error"), JOptionPane.ERROR_MESSAGE);
+          return;
+        }
+        WorkflowDesigner.INSTANCE.openWorkspace(loadField.getFile());
+        dispose();
+      }
+      else
+      {
+        File file = createField.getFile();
+        if(file == null || file.isDirectory())
+        {
+          JOptionPane.showMessageDialog((Component)e.getSource(), ResourceManager.getString("error.file.invalid"), ResourceManager.getString("error"), JOptionPane.ERROR_MESSAGE);
+          return;
+        }
+        Workspace space = WorkflowDesigner.INSTANCE.newWorkspace();
+        space.setLocation(file);
+        Prefs.INSTANCE.put(Prefs.LAST_WORKSPACE, file.toString());
+        WorkflowDesigner.INSTANCE.navigator().setWorkspace(space);
+      }
+      dispose();
+    }
+    else
+    {
+      dispose();
+    }
+  }
+}
