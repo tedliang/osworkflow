a76576be702c6ce7c5097a7173011004025af8a9
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2003-12-28 21:11:41 +0000

    Use i18n name for functions in tables

--- src/designer/com/opensymphony/workflow/designer/editor/StepEditor.java
+++ src/designer/com/opensymphony/workflow/designer/editor/StepEditor.java
@@ -95,12 +95,14 @@ public class StepEditor extends DetailPanel implements ActionListener
     builder.addSeparator(ResourceManager.getString("prefunctions"), cc.xywh(2, 18, 3, 1));
 
     pre = new JTable(preModel);
+    preModel.setGraphModel(getModel());
     builder.add(UIFactory.createTablePanel(pre), cc.xywh(2, 20, 3, 1));
     builder.add(UIFactory.getButtonBar(this, "pre", BUTTONS), cc.xywh(2, 21, 3, 1));
 
     builder.addSeparator(ResourceManager.getString("postfunctions"), cc.xywh(2, 23, 3, 1));
 
     post = new JTable(postModel);
+    postModel.setGraphModel(getModel());
     builder.add(UIFactory.createTablePanel(post), cc.xywh(2, 25, 3, 1));
     builder.add(UIFactory.getButtonBar(this, "post", BUTTONS), cc.xywh(2, 26, 3, 1));
 
