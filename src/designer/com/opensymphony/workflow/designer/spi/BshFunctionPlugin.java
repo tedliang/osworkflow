0336e8d86f3ec98aca710887c0eea51bdb61931c
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2004-01-06 19:16:22 +0000

    Added ability to edit bsh scripts

---
+++ src/designer/com/opensymphony/workflow/designer/spi/BshFunctionPlugin.java
@@ -0,0 +1,37 @@
+package com.opensymphony.workflow.designer.spi;
+
+import java.util.Map;
+
+import com.opensymphony.workflow.loader.ConfigFunctionDescriptor;
+import com.opensymphony.workflow.designer.dialogs.DialogUtils;
+
+/**
+ * User: Hani Suleiman
+ * Date: Jan 6, 2004
+ * Time: 1:23:42 PM
+ */
+public class BshFunctionPlugin implements FunctionPlugin
+{
+	private ConfigFunctionDescriptor function;
+
+	public void setFunction(ConfigFunctionDescriptor descriptor)
+	{
+		this.function = descriptor;
+	}
+
+	public ConfigFunctionDescriptor getFunction()
+	{
+		return function;
+	}
+
+	public boolean editFunction(Map args)
+	{
+		String text = DialogUtils.getTextDialog((String)function.getArgs().get("script"));
+		if(text!=null)
+		{
+			function.getArgs().put("script", text);
+			return true;
+		}
+		return false;
+	}
+}
