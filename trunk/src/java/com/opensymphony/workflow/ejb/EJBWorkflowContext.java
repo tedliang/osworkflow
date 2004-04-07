a389833aedf094561ab968d5c37c6c929ce459b1
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2004-04-07 20:17:21 +0000

    Added ability to plug in context

--- trunk/src/java/com/opensymphony/workflow/ejb/EJBWorkflowContext.java
+++ trunk/src/java/com/opensymphony/workflow/ejb/EJBWorkflowContext.java
@@ -2,12 +2,6 @@
  * Copyright (c) 2002-2003 by OpenSymphony
  * All rights reserved.
  */
-/*
- * Created by IntelliJ IDEA.
- * User: plightbo
- * Date: Apr 29, 2002
- * Time: 10:55:22 PM
- */
 package com.opensymphony.workflow.ejb;
 
 import com.opensymphony.workflow.WorkflowContext;
@@ -19,18 +13,12 @@ import javax.ejb.SessionContext;
  * DOCUMENT ME!
  *
  * @author $author$
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class EJBWorkflowContext implements WorkflowContext {
     //~ Instance fields ////////////////////////////////////////////////////////
 
     private SessionContext context;
-
-    //~ Constructors ///////////////////////////////////////////////////////////
-
-    public EJBWorkflowContext(SessionContext context) {
-        this.context = context;
-    }
 
     //~ Methods ////////////////////////////////////////////////////////////////
 
@@ -40,5 +28,9 @@ public class EJBWorkflowContext implements WorkflowContext {
 
     public void setRollbackOnly() {
         context.setRollbackOnly();
+    }
+
+    public void setSessionContext(SessionContext context) {
+        this.context = context;
     }
 }
