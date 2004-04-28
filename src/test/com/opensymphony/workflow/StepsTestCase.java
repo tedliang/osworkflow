651ded88c7ca9fc3bf03ff1c4c2a9501ba0ebb38
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2004-04-28 00:19:03 +0000

    Added support for step pre and post functions.

--- src/test/com/opensymphony/workflow/StepsTestCase.java
+++ src/test/com/opensymphony/workflow/StepsTestCase.java
@@ -19,23 +19,22 @@ import java.util.*;
  * Date: Oct 14, 2003
  * Time: 3:02:08 PM
  */
-public class JoinSplitsTestCase extends TestCase {
+public class StepsTestCase extends TestCase {
     //~ Instance fields ////////////////////////////////////////////////////////
 
     private AbstractWorkflow workflow;
 
     //~ Constructors ///////////////////////////////////////////////////////////
 
-    public JoinSplitsTestCase(String s) {
+    public StepsTestCase(String s) {
         super(s);
     }
 
     //~ Methods ////////////////////////////////////////////////////////////////
 
     public void testEarlyJoin() throws Exception {
-        Map inputs = new HashMap();
         URL url = getClass().getResource("/samples/earlyjoin.xml");
-        long id = workflow.initialize(url.toString(), 1, inputs);
+        long id = workflow.initialize(url.toString(), 1, null);
         List currentSteps = workflow.getCurrentSteps(id);
         assertEquals("Unexpected number of current steps", 2, currentSteps.size());
         workflow.doAction(id, 1, Collections.EMPTY_MAP);
@@ -49,9 +48,8 @@ public class JoinSplitsTestCase extends TestCase {
     }
 
     public void testSplitCompletedHistorySteps() throws Exception {
-        Map inputs = new HashMap();
         URL url = getClass().getResource("/samples/joinsplit.xml");
-        long id = workflow.initialize(url.toString(), 1, inputs);
+        long id = workflow.initialize(url.toString(), 1, null);
         List currentSteps = workflow.getCurrentSteps(id);
         assertEquals("Unexpected number of current steps", 1, currentSteps.size());
         workflow.doAction(id, 1, Collections.EMPTY_MAP);
@@ -69,6 +67,19 @@ public class JoinSplitsTestCase extends TestCase {
         currentSteps = workflow.getCurrentSteps(id);
         assertEquals("Unexpected number of current steps", 1, currentSteps.size());
         assertEquals("Unexpected number of history steps", 2, historySteps.size());
+    }
+
+    public void testStepPostFunction() throws Exception {
+        URL url = getClass().getResource("/samples/step-post.xml");
+        long id = workflow.initialize(url.toString(), 1, null);
+        workflow.doAction(id, 2, null);
+        assertTrue("post-function was not called as expected", "postvalue".equals(workflow.getPropertySet(id).getString("postkey")));
+    }
+
+    public void testStepPreFunction() throws Exception {
+        URL url = getClass().getResource("/samples/step-pre.xml");
+        long id = workflow.initialize(url.toString(), 1, null);
+        assertTrue("pre-function was not called as expected", "prevalue".equals(workflow.getPropertySet(id).getString("prekey")));
     }
 
     protected void setUp() throws Exception {
