eb77fc6eddcccd986eccb35d0f22034fdd65c397
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2004-11-07 20:05:48 +0000

    Allow specifying of environment for remote ejb execution

--- trunk/src/java/com/opensymphony/workflow/util/ejb/remote/RemoteEJBCondition.java
+++ trunk/src/java/com/opensymphony/workflow/util/ejb/remote/RemoteEJBCondition.java
@@ -8,13 +8,12 @@ import com.opensymphony.module.propertyset.PropertySet;
 
 import com.opensymphony.workflow.*;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import java.lang.reflect.Method;
 
 import java.rmi.RemoteException;
 
+import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.ejb.EJBHome;
@@ -25,22 +24,43 @@ import javax.rmi.PortableRemoteObject;
 
 
 /**
- *
+ * Condition to check a remote EJB that implements {@link ConditionRemote}.
+ * A required argument for this condition is <code>ejb.location</code>, which specifies
+ * the JNDI location to look up the remote EJB at.
+ * <p>
+ * Note that by default, the EJB will be looked up from a default InitialContext. It is
+ * however possible to specify an environment for the initial context by specifying the
+ * standard JNDI keys as arguments.
+ * <p>
+ * For example, specifying an argument with a name of <code>java.naming.security.principal</code>
+ * and a value of <code>testuser</code> will result in an InitialContext being created with
+ * the user specified as 'testuser'.
  *
  * @author $Author: hani $
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class RemoteEJBCondition implements Condition {
-    //~ Static fields/initializers /////////////////////////////////////////////
-
-    private static final Log log = LogFactory.getLog(RemoteEJBCondition.class);
-
     //~ Methods ////////////////////////////////////////////////////////////////
 
     public boolean passesCondition(Map transientVars, Map args, PropertySet ps) throws WorkflowException {
         String ejbLocation = (String) args.get(AbstractWorkflow.EJB_LOCATION);
 
-        ConditionRemote sessionBean = null;
+        Hashtable env = null;
+        Iterator iter = args.entrySet().iterator();
+
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+
+            if (env == null) {
+                env = new Hashtable();
+            }
+
+            if (((String) entry.getKey()).startsWith("java.naming.")) {
+                env.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        ConditionRemote sessionBean;
 
         try {
             EJBHome home = (EJBHome) PortableRemoteObject.narrow(new InitialContext().lookup(ejbLocation), EJBHome.class);
