90634f6f6192d2396ef47d9868e3fb8f43980688
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2004-12-17 18:19:21 +0000

    spring workflow factory

---
+++ trunk/src/java/com/opensymphony/workflow/loader/SpringWorkflowFactory.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.workflow.loader;
+
+import com.opensymphony.workflow.FactoryException;
+
+import java.util.Properties;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision: 1.1 $
+ */
+public class SpringWorkflowFactory extends XMLWorkflowFactory {
+    //~ Instance fields ////////////////////////////////////////////////////////
+
+    private String resource;
+
+    //~ Constructors ///////////////////////////////////////////////////////////
+
+    public SpringWorkflowFactory() {
+        super();
+    }
+
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+    public void setReload(String reload) {
+        this.reload = Boolean.valueOf(reload).booleanValue();
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public void init() {
+        try {
+            Properties props = new Properties();
+            props.setProperty("reload", getReload());
+            props.setProperty("resource", getResource());
+
+            super.init(props);
+            initDone();
+        } catch (FactoryException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getReload() {
+        return String.valueOf(reload);
+    }
+
+    private String getResource() {
+        return resource;
+    }
+}
