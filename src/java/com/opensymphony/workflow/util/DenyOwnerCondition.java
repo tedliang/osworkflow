2e2274b47dc49578ba093efab7b23addcaaf6998
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2005-03-24 11:14:58 +0000

    Fix for WF-382

--- src/java/com/opensymphony/workflow/util/DenyOwnerCondition.java
+++ src/java/com/opensymphony/workflow/util/DenyOwnerCondition.java
@@ -6,58 +6,29 @@ package com.opensymphony.workflow.util;
 
 import com.opensymphony.module.propertyset.PropertySet;
 
-import com.opensymphony.util.TextUtils;
-
 import com.opensymphony.workflow.Condition;
 import com.opensymphony.workflow.StoreException;
-import com.opensymphony.workflow.WorkflowContext;
-import com.opensymphony.workflow.spi.Step;
-import com.opensymphony.workflow.spi.WorkflowEntry;
-import com.opensymphony.workflow.spi.WorkflowStore;
 
-import java.util.*;
+import java.util.Map;
 
 
 /**
- * Simple utility condition that returns false if the owner is the caller. Looks at
- * ALL current steps unless a stepId is given in the optional argument "stepId".
+ * @deprecated Use IsUserOwnerCondition instead
+ * @see com.opensymphony.workflow.util.IsUserOwnerCondition
  *
- * @author <a href="mailto:plightbo@hotmail.com">Pat Lightbody</a>
+ * Simple utility condition that returns false if the owner is the caller. Looks
+ * at ALL current steps unless a stepId is given in the optional argument
+ * "stepId".
+ *
+ * @author <a href="mailto:plightbo@hotmail.com">Pat Lightbody </a>
  */
 public class DenyOwnerCondition implements Condition {
     //~ Methods ////////////////////////////////////////////////////////////////
 
+    // ////////////////////////////////////////////////////////////////
     public boolean passesCondition(Map transientVars, Map args, PropertySet ps) throws StoreException {
-        int stepId = TextUtils.parseInt((String) args.get("stepId"));
-        WorkflowContext context = (WorkflowContext) transientVars.get("context");
-        WorkflowEntry entry = (WorkflowEntry) transientVars.get("entry");
-        WorkflowStore store = (WorkflowStore) transientVars.get("store");
-        List currentSteps = store.findCurrentSteps(entry.getId());
+        IsUserOwnerCondition isOwnerCondition = new IsUserOwnerCondition();
 
-        if (stepId == 0) {
-            for (Iterator iterator = currentSteps.iterator();
-                    iterator.hasNext();) {
-                Step step = (Step) iterator.next();
-
-                if (context.getCaller().equals(step.getOwner())) {
-                    return false;
-                }
-            }
-        } else {
-            for (Iterator iterator = currentSteps.iterator();
-                    iterator.hasNext();) {
-                Step step = (Step) iterator.next();
-
-                if (stepId == step.getStepId()) {
-                    if (context.getCaller().equals(step.getOwner())) {
-                        return false;
-                    } else {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return true;
+        return !(isOwnerCondition.passesCondition(transientVars, args, ps));
     }
 }
