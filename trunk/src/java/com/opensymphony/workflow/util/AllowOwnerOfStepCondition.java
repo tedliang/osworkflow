fab05058cbcd776f619429850ad8dfb3ba23946a
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2005-03-24 11:14:58 +0000

    Fix for WF-382

--- trunk/src/java/com/opensymphony/workflow/util/AllowOwnerOfStepCondition.java
+++ trunk/src/java/com/opensymphony/workflow/util/AllowOwnerOfStepCondition.java
@@ -6,60 +6,28 @@ package com.opensymphony.workflow.util;
 
 import com.opensymphony.module.propertyset.PropertySet;
 
-import com.opensymphony.workflow.*;
-import com.opensymphony.workflow.spi.Step;
-import com.opensymphony.workflow.spi.WorkflowEntry;
-import com.opensymphony.workflow.spi.WorkflowStore;
+import com.opensymphony.workflow.Condition;
+import com.opensymphony.workflow.WorkflowException;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 
 /**
+ * @deprecated Use IsUserOwnerCondition instead
+ * @see com.opensymphony.workflow.util.IsUserOwnerCondition
+ *
  * Checks owner of "stepId" in args and compares to current user
  *
- * @author Travis reeder
- * Date: Feb 18, 2003
- * Time: 4:47:00 PM
+ * @author Travis reeder Date: Feb 18, 2003 Time: 4:47:00 PM
  * @version 0.1
  */
 public class AllowOwnerOfStepCondition implements Condition {
     //~ Methods ////////////////////////////////////////////////////////////////
 
+    // ////////////////////////////////////////////////////////////////
     public boolean passesCondition(Map transientVars, Map args, PropertySet ps) throws WorkflowException {
-        String stepIdString = (String) args.get("stepId");
-        WorkflowEntry entry = (WorkflowEntry) transientVars.get("entry");
-        WorkflowContext context = (WorkflowContext) transientVars.get("context");
+        IsUserOwnerCondition isOwnerCondition = new IsUserOwnerCondition();
 
-        if (stepIdString == null) {
-            throw new WorkflowException("This condition expects a stepId!");
-        }
-
-        int stepId = 0;
-
-        if (stepIdString != null) {
-            try {
-                stepId = Integer.parseInt(stepIdString);
-            } catch (Exception ex) {
-                throw new WorkflowException("This condition expects a stepId > 0!");
-            }
-        }
-
-        WorkflowStore store = (WorkflowStore) transientVars.get("store");
-        List historySteps = store.findHistorySteps(entry.getId());
-
-        for (Iterator iterator = historySteps.iterator(); iterator.hasNext();) {
-            Step step = (Step) iterator.next();
-
-            if (stepId == (step.getStepId())) {
-                // check to see if owner same as caller
-                if (((step.getOwner() != null) && (context.getCaller() != null)) && context.getCaller().equals(step.getOwner())) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        return isOwnerCondition.passesCondition(transientVars, args, ps);
     }
 }
--- trunk/src/java/com/opensymphony/workflow/util/AllowOwnerOnlyCondition.java
+++ trunk/src/java/com/opensymphony/workflow/util/AllowOwnerOnlyCondition.java
@@ -8,61 +8,27 @@ import com.opensymphony.module.propertyset.PropertySet;
 
 import com.opensymphony.workflow.Condition;
 import com.opensymphony.workflow.StoreException;
-import com.opensymphony.workflow.WorkflowContext;
-import com.opensymphony.workflow.spi.Step;
-import com.opensymphony.workflow.spi.WorkflowEntry;
-import com.opensymphony.workflow.spi.WorkflowStore;
 
-import java.util.*;
+import java.util.Map;
 
 
 /**
- * Simple utility condition that returns true if the owner is the caller. Looks at
- * ALL current steps unless a stepId is given in the optional argument "stepId".
+ * @deprecated Use IsUserOwnerCondition instead
+ * @see com.opensymphony.workflow.util.IsUserOwnerCondition
  *
- * @author <a href="mailto:plightbo@hotmail.com">Pat Lightbody</a>
+ * Simple utility condition that returns true if the owner is the caller. Looks
+ * at ALL current steps unless a stepId is given in the optional argument
+ * "stepId".
+ *
+ * @author <a href="mailto:plightbo@hotmail.com">Pat Lightbody </a>
  */
 public class AllowOwnerOnlyCondition implements Condition {
     //~ Methods ////////////////////////////////////////////////////////////////
 
+    // ////////////////////////////////////////////////////////////////
     public boolean passesCondition(Map transientVars, Map args, PropertySet ps) throws StoreException {
-        int stepId = 0;
-        String stepIdVal = (String) args.get("stepId");
+        IsUserOwnerCondition isOwnerCondition = new IsUserOwnerCondition();
 
-        if (stepIdVal != null) {
-            try {
-                stepId = Integer.parseInt(stepIdVal);
-            } catch (Exception ex) {
-            }
-        }
-
-        WorkflowContext context = (WorkflowContext) transientVars.get("context");
-        WorkflowEntry entry = (WorkflowEntry) transientVars.get("entry");
-        WorkflowStore store = (WorkflowStore) transientVars.get("store");
-        List currentSteps = store.findCurrentSteps(entry.getId());
-
-        if (stepId == 0) {
-            for (Iterator iterator = currentSteps.iterator();
-                    iterator.hasNext();) {
-                Step step = (Step) iterator.next();
-
-                if ((step.getOwner() != null) && context.getCaller().equals(step.getOwner())) {
-                    return true;
-                }
-            }
-        } else {
-            for (Iterator iterator = currentSteps.iterator();
-                    iterator.hasNext();) {
-                Step step = (Step) iterator.next();
-
-                if (stepId == step.getStepId()) {
-                    if ((step.getOwner() != null) && context.getCaller().equals(step.getOwner())) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
+        return isOwnerCondition.passesCondition(transientVars, args, ps);
     }
 }
