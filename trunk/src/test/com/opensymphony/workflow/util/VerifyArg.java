11d86772a81849f611ad2af2299b1cc741153440
hani <hani@e4ae6f55-34ee-e699-890d-fb0a57692035>
2005-04-05 01:06:30 +0000

    Added test to verify args are reevaluated for every usage

---
+++ trunk/src/test/com/opensymphony/workflow/util/VerifyArg.java
@@ -0,0 +1,22 @@
+package com.opensymphony.workflow.util;
+
+import java.util.Map;
+
+import com.opensymphony.workflow.FunctionProvider;
+import com.opensymphony.workflow.WorkflowException;
+import com.opensymphony.module.propertyset.PropertySet;
+import junit.framework.TestCase;
+
+/**
+ * @author hani Date: Apr 4, 2005 Time: 8:56:36 PM
+ */
+public class VerifyArg implements FunctionProvider
+{
+  public void execute(Map transientVars, Map args, PropertySet ps) throws WorkflowException
+  {
+    Object expected = args.get("expected");
+    Object actual = args.get("actual");
+    System.out.println("expected is " + expected + " actual is " + actual);
+    TestCase.assertEquals(expected, actual);
+  }
+}
