5047f03637fe5d3988e04c46c6219d0092acdd47
masini <masini@e4ae6f55-34ee-e699-890d-fb0a57692035>
2005-10-20 22:26:28 +0000

    Now the Impl uses the memory PropertySet

--- trunk/src/java/com/opensymphony/workflow/util/PropertySetDelegateImpl.java
+++ trunk/src/java/com/opensymphony/workflow/util/PropertySetDelegateImpl.java
@@ -14,20 +14,12 @@ import java.util.HashMap;
  * DOCUMENT ME!
  *
  * @author $author$
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class PropertySetDelegateImpl implements PropertySetDelegate {
     //~ Methods ////////////////////////////////////////////////////////////////
 
     public PropertySet getPropertySet(long entryId) {
-        HashMap args = new HashMap();
-        args.put("entityName", "OSWorkflowEntry");
-        args.put("entityId", new Long(entryId));
-
-        //        DefaultHibernateConfigurationProvider configurationProvider = new DefaultHibernateConfigurationProvider();
-        //        //configurationProvider.setSessionFactory(sessionFactory);
-        //
-        //        args.put("configurationProvider", configurationProvider);
-        return PropertySetManager.getInstance("gioia", args);
+        return PropertySetManager.getInstance("memory", new HashMap());
     }
 }
