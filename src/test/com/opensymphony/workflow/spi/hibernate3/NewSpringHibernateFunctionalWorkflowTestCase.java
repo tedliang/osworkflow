0adcee8bee2fc1377000cdfe1f3c14387b582dc8
masini <masini@e4ae6f55-34ee-e699-890d-fb0a57692035>
2005-10-20 22:27:47 +0000

    *** empty log message ***

---
+++ src/test/com/opensymphony/workflow/spi/hibernate3/NewSpringHibernateFunctionalWorkflowTestCase.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+package com.opensymphony.workflow.spi.hibernate3;
+
+import com.opensymphony.workflow.Workflow;
+import com.opensymphony.workflow.config.Configuration;
+import com.opensymphony.workflow.spi.AbstractFunctionalWorkflowTest;
+import com.opensymphony.workflow.util.DatabaseHelper;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+
+import org.springframework.core.io.ClassPathResource;
+
+
+/**
+ * White Box semi-functional test case that uses Spring's managed Hibernate as Store
+ *
+ * @author Luca Masini (l.masini@infogroup.it)
+ */
+public class NewSpringHibernateFunctionalWorkflowTestCase extends AbstractFunctionalWorkflowTest {
+    //~ Constructors ///////////////////////////////////////////////////////////
+
+    public NewSpringHibernateFunctionalWorkflowTestCase(String s) {
+        super(s);
+    }
+
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        DatabaseHelper.runScript("", "jdbc/CreateDS");
+
+        XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("new-osworkflow-spring-hibernate3.xml"));
+
+        workflow = (Workflow) beanFactory.getBean("workflow");
+        workflow.setConfiguration((Configuration) beanFactory.getBean("osworkflowConfiguration"));
+    }
+
+    protected String getWorkflowName() {
+        return "example";
+    }
+}
