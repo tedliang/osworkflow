6d2d5271aaaa888c18e9d689cf22852fcacfcbfb
skanjo <skanjo@e4ae6f55-34ee-e699-890d-fb0a57692035>
2008-06-03 03:48:50 +0000

    PropertySetDelegate supporting hibernate3, which is now available in PropertySet 1.5.

---
+++ trunk/src/java/com/opensymphony/workflow/spi/hibernate3/DefaultHibernatePropertySetDelegate.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2002-2003 by OpenSymphony
+ * All rights reserved.
+ */
+
+/**
+ * Copyright 2008 Tribune Company All rights reserved.
+ * TRIBUNE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.opensymphony.workflow.spi.hibernate3;
+
+import com.opensymphony.module.propertyset.PropertySet;
+import com.opensymphony.module.propertyset.hibernate3.DefaultHibernateConfigurationProvider;
+import com.opensymphony.module.propertyset.hibernate3.HibernatePropertySet;
+
+import com.opensymphony.workflow.util.PropertySetDelegate;
+
+import org.hibernate.SessionFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * DOCUMENTME
+ * @author $author$
+ * @version $Revision: 1.1 $
+ *
+ */
+public class DefaultHibernatePropertySetDelegate implements PropertySetDelegate {
+    //~ Instance fields ////////////////////////////////////////////////////////
+
+    // ~ Instance fields
+    // ////////////////////////////////////////////////////////
+    private SessionFactory sessionFactory;
+
+    //~ Constructors ///////////////////////////////////////////////////////////
+
+    // ~ Constructors
+    // ///////////////////////////////////////////////////////////
+    public DefaultHibernatePropertySetDelegate() {
+        super();
+    }
+
+    //~ Methods ////////////////////////////////////////////////////////////////
+
+    // ~ Methods
+    // ////////////////////////////////////////////////////////////////
+    public PropertySet getPropertySet(long entryId) {
+        HashMap args = new HashMap();
+        args.put("entityName", "OSWorkflowEntry");
+        args.put("entityId", new Long(entryId));
+
+        DefaultHibernateConfigurationProvider configurationProvider = new DefaultHibernateConfigurationProvider();
+        configurationProvider.setSessionFactory(getSessionFactory());
+
+        args.put("configurationProvider", configurationProvider);
+
+        Map config = new HashMap();
+        PropertySet properties = new HibernatePropertySet();
+        properties.init(config, args);
+
+        return properties;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    private SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
